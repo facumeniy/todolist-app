2bd35acd1360d5263773199c5366ed1bb3f88aea
Facundo Meniy <facundomeniy@gmail.com>
2022-12-16 18:24:48 -0300

    storage functionality

--- src/modules/sidebarDOM.js
+++ src/modules/sidebarDOM.js
@@ -1,10 +1,16 @@
 function sidebarDOM(){
     const todoPage = document.querySelector(".main-page");
+    const storage = document.querySelector(".storage");
 
     const menuBtn = document.getElementById("menu-btn");
     const sidebar = document.querySelector(".sidebar");
 
+    const inboxBtn = document.getElementById("inbox-btn");
+    const inboxText = document.getElementById("inbox-text");
     const projectsBtn = document.getElementById("projects-btn");
+    const projectsText = document.getElementById("projects-text");
+
+    let enabled = true;
 
     sidebar.classList.add("hide");
 
@@ -18,11 +24,24 @@ function sidebarDOM(){
         }
     })
 
-    projectsBtn.addEventListener('click', () => {
-        // TODOS HAVE TO BE STORED
-        // PROJECTS TEXT SHOULD CLEAR ALL TODOS
+    inboxText.addEventListener('click', () => {
+        if(enabled === false){
+            todoPage.innerHTML = storage.innerHTML;
+            storage.innerHTML = "";
+            enabled = !enabled;
+        }
+    })
 
+    projectsBtn.addEventListener('click', () => {
         projectsBtn.classList.toggle("rotate");
+    })
+
+    projectsText.addEventListener('click', () => {
+        if(enabled === true){
+            storage.innerHTML = todoPage.innerHTML;
+            todoPage.innerHTML = "";
+            enabled = !enabled;
+        }
     })
 }
 
